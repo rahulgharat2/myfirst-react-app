cb496d84aeccf4716e04cee20f4ae01959a12a98
Rahul Gharat <Rahul.Gharat@harbingergroup.com>
2023-04-14 11:19:39 +0530

    [feature/newExpense] added all newExpense feature required files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
+  const expenses = [
+    { id: 'e1', title: 'car Insurance', amount: 300.69, date: new Date(2021, 3, 26) },
+    { id: 'e2', title: 'New TV', amount: 305.59, date: new Date(2022, 4, 27) },
+    { id: 'e3', title: 'Mobile', amount: 310.10, date: new Date(2023, 5, 28) },
+    { id: 'e4', title: 'Laptop', amount: 315.15, date: new Date(2024, 6, 29) }
+  ];
+
+
+// return React.createElement(
+//   'div',
+//   {},
+//   React.createElement('h2',{},'Lets get Started'),
+//   React.createElement(Expenses,{item:expenses})
+
+// );
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense/>
+      <Expenses item={expenses}/>
     </div>
   );
-}
+ }
 
 export default App;
